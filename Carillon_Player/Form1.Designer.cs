14f4c1577e34b3a8078a7711bbe683fe5f596e98
olabuschagne <ockert@sbcglobal.net>
2024-06-12 15:50:24 -0500

    Added Carillon_Player

---
+++ Carillon_Player/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Carillon_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblNextPlayTime = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblWaveInfo = new System.Windows.Forms.Label();
+            this.lblPlayList = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipTitle = "Carillon Player";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Carillon Player";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipClosed += new System.EventHandler(this.notifyIcon1_BalloonTipClosed);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            this.notifyIcon1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseMove);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::Carillon_Player.Properties.Resources.Aqua_Dot_32;
+            this.panel1.Location = new System.Drawing.Point(218, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(32, 32);
+            this.panel1.TabIndex = 17;
+            // 
+            // lblNextPlayTime
+            // 
+            this.lblNextPlayTime.AutoSize = true;
+            this.lblNextPlayTime.Location = new System.Drawing.Point(27, 55);
+            this.lblNextPlayTime.Name = "lblNextPlayTime";
+            this.lblNextPlayTime.Size = new System.Drawing.Size(62, 13);
+            this.lblNextPlayTime.TabIndex = 16;
+            this.lblNextPlayTime.Text = "Not running";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Time until next play:";
+            // 
+            // lblWaveInfo
+            // 
+            this.lblWaveInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblWaveInfo.Location = new System.Drawing.Point(27, 107);
+            this.lblWaveInfo.Name = "lblWaveInfo";
+            this.lblWaveInfo.Size = new System.Drawing.Size(230, 139);
+            this.lblWaveInfo.TabIndex = 14;
+            // 
+            // lblPlayList
+            // 
+            this.lblPlayList.AutoSize = true;
+            this.lblPlayList.Location = new System.Drawing.Point(27, 81);
+            this.lblPlayList.Name = "lblPlayList";
+            this.lblPlayList.Size = new System.Drawing.Size(0, 13);
+            this.lblPlayList.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Wave File";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Play List";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(108, 16);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 10;
+            this.btnStop.Text = "Exit";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(27, 16);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 9;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblNextPlayTime);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblWaveInfo);
+            this.Controls.Add(this.lblPlayList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Carillon Player";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblNextPlayTime;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblWaveInfo;
+        private System.Windows.Forms.Label lblPlayList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Carillon_Player/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Microsoft.DirectX.DirectSound;
+
+using com.olab_it;
+
+namespace Carillon_Player
+{
+    public partial class Form1 : Form
+    {
+        public Form1(bool autoStart = true)
+        {
+            InitializeComponent();
+            player = new CarillonPlayer();
+            player.TimerElapsed += new EventHandler(player_TimerElapsed);
+            timer1.Start();
+            AutoStart = autoStart;
+            Application.DoEvents();
+        }
+
+        bool baloonTipVisible = false,
+            AutoStart;
+        CarillonPlayer player;
+
+        void player_TimerElapsed(object sender, EventArgs e)
+        {
+            UpdateUI();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.notifyIcon1.Visible = true;
+                this.notifyIcon1.ShowBalloonTip(500);
+                baloonTipVisible = true;
+                this.Hide();
+            }
+            else
+            {
+                this.notifyIcon1.Visible = false;
+            }
+            Application.DoEvents();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.notifyIcon1.Visible = false;
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!player.Running)
+                player.Start(this.Handle);
+            UpdateUI();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+            UpdateUI();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(AutoStart && this.Visible)
+            {
+                if (!player.Running)
+                    player.Start(this.Handle);
+                AutoStart = false;
+                this.WindowState = FormWindowState.Minimized;
+            }
+            UpdateUI();
+        }
+
+        private void notifyIcon1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!baloonTipVisible)
+            {
+                TimeSpan span = player.TimeUntilNextPlay;
+                notifyIcon1.BalloonTipText = player.Running ? string.Format("Next Play {0} hours, {1} minutes, {2} seconds", span.Hours, span.Minutes, span.Seconds) : "Not running";
+                this.notifyIcon1.ShowBalloonTip(100);
+                baloonTipVisible = true;
+            }
+        }
+
+        private void notifyIcon1_BalloonTipClosed(object sender, EventArgs e)
+        {
+            baloonTipVisible = false;
+        }
+
+        private void UpdateUI()
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(this.UpdateUI));
+            }
+            else
+            {
+                TimeSpan span = player.TimeUntilNextPlay;
+                this.lblNextPlayTime.Text = player.Running ? string.Format("{0} hours, {1} minutes, {2} seconds", span.Hours, span.Minutes, span.Seconds) : "Not running";
+                notifyIcon1.BalloonTipText = player.Running ? string.Format("Next Play {0} hours, {1} minutes, {2} seconds", span.Hours, span.Minutes, span.Seconds) : "Not running";
+                this.lblPlayList.Text = player.Running ? player.ActivePlayList : "";
+                this.lblWaveInfo.Text = player.Running ? player.WaveDescription : "";
+                this.panel1.BackgroundImage = player.Running ? player.IsPlaying ? global::Carillon_Player.Properties.Resources.Red_Dot_32 :
+                    global::Carillon_Player.Properties.Resources.Green_Dot_32 :
+                    global::Carillon_Player.Properties.Resources.Aqua_Dot_32;
+                Application.DoEvents();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            player.Stop();
+            player.Dispose();
+            timer1.Stop();
+        }
+
+    }
+}
